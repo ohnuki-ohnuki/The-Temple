3ff5ddef3b14afe22b4c12ca38e8c5d5211bec06
ohnuki-ohnuki <ohnukihiroki8585@yahoo.co.jp>
2020-10-11 22:15:35 +0900

    The Temple

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+        private const int MAX_ORB = 10;
+        private const int RESPAWN_TIME = 1;
+        private const int MAX_LEVEL = 2;
+
+        public GameObject orbPrefab;
+        public GameObject smokePrefab;
+        public GameObject kusudamaPrefab;
+        public GameObject canvasGame;
+        public GameObject textScore;
+        public GameObject imageTemple;
+        public GameObject imageMokugyo;
+
+        public AudioClip getScoreSE;
+        public AudioClip levelUpSE;
+        public AudioClip clearSE;
+
+        private int score = 0;
+        private int nextScore = 10;
+
+        private int currentOrb = 0;
+
+        private int templeLevel = 0;
+
+        private DateTime lastDateTime;
+
+        private int[] nextScoreTable = new int[]{10,100,1000};
+
+        private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        currentOrb = 10;
+
+        for (int i = 0;i < currentOrb;i++){
+            CreateOrb ();
+        }
+        lastDateTime = DateTime.UtcNow;
+        nextScore = nextScoreTable [templeLevel];
+        imageTemple.GetComponent<TempleManager>().SetTemplePicture(templeLevel);
+        imageTemple.GetComponent<TempleManager>().SetTempleScale(score,nextScore);    
+        RefreshScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentOrb < MAX_ORB){
+            TimeSpan timeSpan = DateTime.UtcNow - lastDateTime;
+
+            if(timeSpan >= TimeSpan.FromSeconds (RESPAWN_TIME)){
+                while(timeSpan >= TimeSpan.FromSeconds(RESPAWN_TIME)){
+                    CreateNewOrb();
+                    timeSpan -= TimeSpan.FromSeconds(RESPAWN_TIME);
+                }
+            }
+        }
+    }
+    public void CreateNewOrb(){
+        lastDateTime = DateTime.UtcNow;
+        if ( currentOrb >= MAX_ORB){
+            return;
+        }
+        CreateOrb();
+        currentOrb++;
+    }
+
+    public void CreateOrb(){
+        GameObject orb = (GameObject)Instantiate(orbPrefab);
+        orb.transform.SetParent(canvasGame.transform,false);
+        orb.transform.localPosition = new Vector3(
+            UnityEngine.Random.Range(-300.0f,300.0f),
+            UnityEngine.Random.Range(-140.0f,-500.0f),
+            0f);
+
+        int kind = UnityEngine.Random.Range(0,templeLevel + 1);
+        switch (kind) {
+            case 0:
+                  orb.GetComponent<OrbManager>().SetKind(OrbManager.ORB_KIND.BLUE);
+                  break;
+            case 1:
+                  orb.GetComponent<OrbManager>().SetKind(OrbManager.ORB_KIND.GREEN);
+                  break;
+            case 2:
+                  orb.GetComponent<OrbManager>().SetKind(OrbManager.ORB_KIND.PURPLE);
+                  break;
+        }
+    }
+        
+        public void GetOrb(int getScore){
+
+            audioSource.PlayOneShot(getScoreSE);
+            AnimatorStateInfo stateInfo =imageMokugyo.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.fullPathHash==Animator.StringToHash("Base Layer.get@ImageMokugyo")){
+                imageMokugyo.GetComponent<Animator>().Play(stateInfo.fullPathHash,0,0.0f);
+            }else{
+                imageMokugyo.GetComponent<Animator>().SetTrigger("isGetScore");
+            }
+            
+
+            if (score < nextScore){
+                score += getScore;
+            
+        if (score > nextScore){
+            score = nextScore;
+        }
+        TempleLevelUp();
+        RefreshScoreText();
+        imageTemple.GetComponent<TempleManager>().SetTempleScale(score,nextScore);
+        if ((score == nextScore) && (templeLevel == MAX_LEVEL)){
+            ClearEffect();
+        }
+    }
+        currentOrb--;
+    }
+
+    void RefreshScoreText(){
+        textScore.GetComponent<Text>().text =
+        "徳 :" + score + " / " + nextScore;
+    }
+    void TempleLevelUp(){
+        if (score >= nextScore){
+            if(templeLevel < MAX_LEVEL){
+                templeLevel++;
+                score = 0;
+
+                TempleLevelUpEffect();
+
+                nextScore = nextScoreTable[templeLevel];
+                imageTemple.GetComponent<TempleManager>().SetTemplePicture(templeLevel);
+            }
+        }
+    }
+    void TempleLevelUpEffect(){
+        GameObject smoke = (GameObject)Instantiate(smokePrefab);
+        smoke.transform.SetParent(canvasGame.transform,false);
+        smoke.transform.SetSiblingIndex(2);
+
+        audioSource.PlayOneShot(levelUpSE);
+
+        Destroy(smoke,0.5f);
+    }
+
+    void ClearEffect(){
+        GameObject kusudama = (GameObject)Instantiate(kusudamaPrefab);
+        kusudama.transform.SetParent(canvasGame.transform,false);
+
+        audioSource.PlayOneShot(clearSE);
+    }
+
+}
---
+++ Assets/Scripts/OrbManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using DG.Tweening;
+
+public class OrbManager : MonoBehaviour
+{
+    private GameObject gameManager;
+
+    public Sprite[] orbPicture = new Sprite[3];
+
+    public enum ORB_KIND{
+        BLUE,
+        GREEN,
+        PURPLE,
+    }
+    private ORB_KIND orbKind;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TouchOrb(){
+        if (Input.GetMouseButton (0) == false){
+            return;
+        }
+        RectTransform rect = GetComponent<RectTransform>();
+
+        Vector3[] path = {
+            new Vector3(rect.localPosition.x * 1.5f,300f,0f),
+            new Vector3(0f,150f,0f),
+            };
+        
+        rect.DOLocalPath(path,0.5f,PathType.CatmullRom)
+        .SetEase(Ease.OutQuad)
+        .OnComplete(AddOrbPoint);
+        rect.DOScale(
+            new Vector3(0.5f,0.5f,0f),
+            0.5f
+            );
+
+        }
+    
+    void AddOrbPoint(){
+        switch (orbKind)
+        {
+            case ORB_KIND.BLUE:
+                gameManager.GetComponent<GameManager> ().GetOrb (1);
+                break;
+            case ORB_KIND.GREEN:
+                gameManager.GetComponent<GameManager> ().GetOrb (10);
+                break;
+            case ORB_KIND.PURPLE:
+                gameManager.GetComponent<GameManager> ().GetOrb (100);
+                break;
+        }
+    
+       
+        Destroy(this.gameObject);
+    }
+
+    public void SetKind (ORB_KIND kind){
+        orbKind = kind;
+        switch (orbKind){
+            case ORB_KIND.BLUE:
+                GetComponent<Image>().sprite = orbPicture[0];
+                break;
+            case ORB_KIND.GREEN:
+                GetComponent<Image>().sprite = orbPicture[1];
+                break;
+            case ORB_KIND.PURPLE:
+                GetComponent<Image>().sprite = orbPicture[2];
+                break;
+
+        }
+    }
+}
